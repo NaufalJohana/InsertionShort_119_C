c6d5564eba469ff4dbf377424db8742599bac03f
NaufalJohana <naufaljohana@gmail.com>
2023-03-23 15:02:55 +0700

    Membuat tampilan data element Array

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -5,15 +5,21 @@ int arr[20];	// Membuat array dengan panjang data 20
 int n;			//  Membuat variable inputan n
 
 void input()
+	// Procedure Input
+{while (true)
 {
-	while (true)
-	{
 		cout << "Memasukkan Jumlah Data pada Array : ";	// Membuat inputan julah element array
-		cin >> n;
-
+		cin >> n;										// Memanggil variable inputan n
 		if (n <= 20)
-			(	// Membuar kondisi n tidak lebih dari 20
+		{	// Membuar kondisi n tidak lebih dari 20
+				break;
 
-			)
+		}
+		else
+		{
+			cout << "\n Arrray yang anda masukkan maksimal 20 elemen. \n"	// Menampilkan pesan jika data
+		}
 	}
 }
+cout << endl;												// Membuat jarak per baris progan
+cout << "===========================" << endl;					// Membuat tampilan data element Array
