dcefa30583e127e22a6e97d3894cffb5b7aef503
NaufalJohana <naufaljohana@gmail.com>
2023-03-23 15:16:09 +0700

    prosedure insertionshort

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -20,6 +20,30 @@ void input()
 			cout << "\n Arrray yang anda masukkan maksimal 20 elemen. \n"	// Menampilkan pesan jika data
 		}
 	}
+
 }
-cout << endl;												// Membuat jarak per baris progan
-cout << "===========================" << endl;					// Membuat tampilan data element Array
+	cout << endl;												// Membuat jarak per baris program
+	cout << "===========================" << endl;					// Membuat tampilan data element Array
+	cout << "Masukan Element Array" << endl;
+	cout << "===========================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << " data ke-n" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+// prosedure insertionshort
+void insertionshort()
+}
+	int temp// membuat variable daya temporer atau penyimpanan sementara
+	int j; // membuat variable j sebagai panda
+
+	for (int i = 1; i < n; i++) // membuat loping dengan i di mulai dari 1 hinggs n-1
+	{
+		temp = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+		}
+	arr[j + 1] = arr[j];
